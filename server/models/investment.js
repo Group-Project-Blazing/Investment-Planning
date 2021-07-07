146421214c365aea5b46cc18cd7e938281e9d270
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-08 03:05:23 +0700

    adjustment from arip code to investment planning project code - server

---
+++ server/models/investment.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Investment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, {
+        foreignKey: "UserId",
+      });
+    }
+  }
+  Investment.init(
+    {
+      UserId: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Investment",
+    }
+  );
+  return Investment;
+};
